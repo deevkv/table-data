076858e4cf3b2089f8aa1b7f82df3e32a9116ee7
deevkv <deev.kv@gmail.com>
2020-03-02 02:46:25 +0300

    Adding a modal window. Adding modal window shape validation. Adding table overwrite function. Adding filter method for date and time.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
    <v-app id="inspire">
     <v-content>
       <v-container
-        class="fill-height container"
+        class="fill-height app-container"
         fluid
       >
         <v-row
@@ -16,34 +16,115 @@
             <v-data-table
               :headers="headers"
               :items="desserts"
-              item-key="ordNumber"
-              class="pa-3 elevation-1" 
-            > 
-             <!-- <template v-slot:item="props">
-                <tr>
-                 <td class="text-left pa-3">{{ props.item.ordNumber }}</td>
-                 <td class="text-left">{{ props.item.carNumber }}</td>
-                 <td class="text-xs-right">{{ props.item.trainIndex }}</td>
-                 <td class="text-center">{{ props.item.trainNumber }}</td>
-                 <td class="text-xs-right">{{ props.item.carStatus }}</td>
-                 <td class="text-xs-right" >{{ props.item.lastOperDt }}</td>
-                 <td class="text-xs-right">{{ props.item.invoiceNumber }}</td>
-                 <td class="text-xs-right">{{ props.item.invoiceId }}</td>
-                 <td>{{ props.item.stateId }}</td>
-               </tr>  
-             </template> -->
+              sort-by="calories"
+              class="elevation-1"
+              mobile-breakpoint="767"
+              @click:row ="editItem"
+            >
+              <template v-slot:top>
+                  <v-dialog v-model="dialog" max-width="500px"> 
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Внести изменения</span>
+                      </v-card-title>
+
+                      <v-card-text>
+                        <v-form v-model="valid" ref="form">   
+                          <v-container> 
+                            <v-row>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field color="red" disabled v-model="editedItem.ordNumber" label="№ п/п"></v-text-field>
+                              </v-col>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field color="red" disabled v-model="editedItem.carNumber" label="Номер вагона"></v-text-field>
+                              </v-col>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field color="red" disabled v-model="editedItem.trainIndex" label="Индекс поезда"></v-text-field>
+                              </v-col>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field 
+                                  color="black" 
+                                  v-model="editedItem.trainNumber" 
+                                  label="Номер поезда"
+                                  :rules="trainNumberRules"
+                                  :counter="4"
+                                  type="number"
+                                  required
+                                > 
+                                </v-text-field>
+                              </v-col>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field color="red" disabled v-model="editedItem.carStatus" label="Статус"></v-text-field>
+                              </v-col>
+                              <v-col cols="12" sm="6" md="6">
+                                <v-text-field 
+                                  color="black"
+                                  v-model="editedItem.invoiceId"
+                                  label="ИД Накладной"
+                                  :rules="invoiceIdRules"
+                                  :counter="9"
+                                  type="number"
+                                  required
+                                >
+                                </v-text-field>
+                              </v-col>
+                               <v-col cols="12" sm="6" md="6">
+                                <v-text-field 
+                                  color="black" 
+                                  v-model="editedItem.invoiceNumber" 
+                                  label="№ Накладной"
+                                  :rules="invoiceNumberRules"
+                                  :counter="8"
+                                > 
+                                </v-text-field>
+                              </v-col>
+                               <v-col cols="12" sm="6" md="6">
+                                <v-text-field color="red" disabled v-model="editedItem.stateId" label="state id"></v-text-field>
+                              </v-col>
+                               <v-col cols="12" sm="6" md="6">
+                                <v-text-field 
+                                  color="black" 
+                                  v-model="editedItem.lastOperDt" 
+                                  label="Дата-время операции"
+                                  :rules="lastOperDtRules"
+                                  hint="ДД/ММ/ГГГГ"
+                                  
+                                  required
+                                >
+                                </v-text-field>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-form>  
+                      </v-card-text>
+
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn 
+                          color="red darken-1" 
+                          text @click="save"
+                          :disabled="!valid"
+                        >OK</v-btn>
+                        <v-btn color="red darken-1" text @click="close">Отмена</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+              </template>
               <template v-slot:item.lastOperDt="{ item }">
-                <div v-if="item.lastOperDt">{{new Date(item.lastOperDt).toLocaleString("ru-RU", 
+                <span v-if="item.lastOperDt">{{new Date(item.lastOperDt).toLocaleString("ru-RU", 
                   {
-                      year: 'numeric',
-                      month: 'numeric',
-                      day: 'numeric',
-                      hour: 'numeric',
-                      minute: 'numeric'
+                    year: 'numeric',
+                    month: 'numeric',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric'
                   }
-                )}}</div>
-                <div v-else><p class="text-sm-center">-</p></div>
-             </template>
+                )}}</span>
+                <span v-else>-</span>
+              </template>  
+              <template v-slot:no-data>
+                <v-btn color="primary" @click="initialize">Reset</v-btn>
+              </template>
             </v-data-table>
           </v-col>
         </v-row>
@@ -53,22 +134,101 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          { text: '№ п/п', value: 'ordNumber'},
-          { text: 'Номер вагона', value: 'carNumber' },
-          { text: 'Индекс поезда', value: 'trainIndex' },
-          { text: 'Номер поезда', align: 'center', value: 'trainNumber' },
-          { text: 'Статус', align: 'center', value: 'carStatus' },
-          { text: 'Дата-время операции', align: 'center', value: 'lastOperDt' },
-          { text: '№ Накладной', value: 'invoiceNumber' },
-          { text: 'ИД Накладной', value: 'invoiceId' },
-          { text: 'state id', value: 'stateId' }
+    export default {
+    data: () => ({
+      dialog: false,
+      valid: false,
+      trainNumberRules: [
+        v => !!v || 'Требуется номер поезда',
+        v => (v.length >= 4 && v.length <= 4) ||  'Номер должен быть равен 4 символам'
+      ],
+      invoiceIdRules: [
+        v => !!v || 'Требуется ИД накладной',
+        v => (v.length >= 9 && v.length <= 9) ||  'ИД накладной долджно быть 9 символов'
+      ],
+      invoiceNumberRules: [
+        v => !!v || 'Требуется номер накладной',
+        v => v.length >= 8 ||  'Name must be less than 8 characters'
+      ],
+      lastOperDtRules: [
+        v => !!v || 'Требуется дата',
+        v => v.length >= 8 ||  'Name must be less than 8 characters'
+      ],
+      headers: [
+        { text: '№ п/п', align: 'end', value: 'ordNumber'},
+        { text: 'Номер вагона', value: 'carNumber' },
+        { text: 'Индекс поезда', value: 'trainIndex' },
+        { text: 'Номер поезда', align: 'center', value: 'trainNumber' },
+        { text: 'Статус', align: 'center', value: 'carStatus' },
+        { text: 'Дата-время операции', align: 'center', value: 'lastOperDt' },
+        { text: '№ Накладной', value: 'invoiceNumber' },
+        { text: 'ИД Накладной', align: 'end', value: 'invoiceId' },
+        { text: 'state id', align: 'end', value: 'stateId' },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        ordNumber: 0,
+        carNumber: 0,
+        trainIndex: 0,
+        trainNumber: 0,
+        carStatus: '',
+        invoiceId: 0,
+        invoiceNumber: '',
+        stateId: 0,
+        lastOperDt: 0
+      },
+      defaultItem: {
+        ordNumber: 0,
+        carNumber: 0,
+        trainIndex: 0,
+        trainNumber: 0,
+        carStatus: '',
+        invoiceId: 0,
+        invoiceNumber: '',
+        stateId: 0,
+        lastOperDt: 0
+      },
+    }),
 
-        ],
-        desserts: [
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            trainNumber: this.editedItem.trainNumber
+          }
+          window.console.log(user) 
+        }
+      },
+      formatDataTime(value) {
+        if(value) {
+          value = new Date(value).toLocaleString("ru-RU", 
+            {
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric'
+            }
+          )
+
+        } else {
+          value = '-'
+        }
+        return value;
+      },
+      initialize () {
+        this.desserts = [
           {
               "ordNumber": 1,
               "carNumber": "94003894",
@@ -101,117 +261,62 @@
               "invoiceNumber": "ЭО730743",
               "stateId": 3,
               "lastOperDt": null
-          },
-          {
-              "ordNumber": 4,
-              "carNumber": "94950078",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 3,
-              "lastOperDt": null
-          },
-          {
-              "ordNumber": 5,
-              "carNumber": "54964036",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 3,
-              "lastOperDt": null
-          },
-          {
-              "ordNumber": 6,
-              "carNumber": "94949708",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 3,
-              "lastOperDt": null
-          },
-          {
-              "ordNumber": 7,
-              "carNumber": "94395381",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 3,
-              "lastOperDt": null
-          },
-          {
-              "ordNumber": 8,
-              "carNumber": "94420957",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "осмотрен в техническом отношении",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 35,
-              "lastOperDt": "2019-08-14T10:04:00.000Z"
-          },
-          {
-              "ordNumber": 9,
-              "carNumber": "94693793",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "осмотрен в техническом отношении",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 35,
-              "lastOperDt": "2019-08-14T10:04:00.000Z"
-          },
-          {
-              "ordNumber": 10,
-              "carNumber": "59910554",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "осмотрен в техническом отношении",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 35,
-              "lastOperDt": "2019-08-14T14:22:00.000Z"
-          },
-          {
-              "ordNumber": 11,
-              "carNumber": "94949773",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "осмотрен в техническом отношении",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 35,
-              "lastOperDt": "2019-07-28T00:30:00.000Z"
-          },
-          {
-              "ordNumber": 12,
-              "carNumber": "94968922",
-              "trainIndex": null,
-              "trainNumber": null,
-              "carStatus": "осмотрен в техническом отношении",
-              "invoiceId": "924094926",
-              "invoiceNumber": "ЭО730743",
-              "stateId": 35,
-              "lastOperDt": "2019-08-14T10:04:00.000Z"
           }
-        ],
-        methods: {
-          
-        },
-      }
-    }
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        let formatData =  this.formatDataTime(item.lastOperDt)
+        this.editedItem.lastOperDt = formatData;
+        this.dialog = true
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        this.onSubmit(); 
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        this.close()
+      },
+    },
   }
 </script>
 
 <style>
-  .container {
+  .app-container {
     background: #f6f6f6; 
   }
+  /* 
+  .table hr {
+    text-align: start !important;
+  }
+  .table td:first-child  {
+    padding-right: 2.8em !important;
+  }
+  
+  .table td:last-child  {
+    padding-right: 2.8em !important;
+  } */
 </style>
+
+
+<!-- <template v-slot:item.lastOperDt="{ item }">
+                <div v-if="item.lastOperDt">{{new Date(item.lastOperDt).toLocaleString("ru-RU", 
+                  {
+                      year: 'numeric',
+                      month: 'numeric',
+                      day: 'numeric',
+                      hour: 'numeric',
+                      minute: 'numeric'
+                  }
+                )}}</div>
+                <div v-else><p class="text-sm-center">-</p></div>
+             </template> -->
